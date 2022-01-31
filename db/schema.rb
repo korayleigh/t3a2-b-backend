69e9a27347d595a46ebee74bef4a85385906424c
leighpoulier <86460742+leighpoulier@users.noreply.github.com>
2022-01-31 20:35:41 +1100

    add fields first_name and last_name to User model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_31_091545) do
+ActiveRecord::Schema.define(version: 2022_01_31_093407) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -80,6 +80,8 @@ ActiveRecord::Schema.define(version: 2022_01_31_091545) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "userable_type"
     t.bigint "userable_id"
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["userable_type", "userable_id"], name: "index_users_on_userable"
