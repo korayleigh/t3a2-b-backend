25b55293542e60977513b86e31605f80538ec2e5
leighpoulier <86460742+leighpoulier@users.noreply.github.com>
2022-01-31 19:51:18 +1100

    Devise User setup and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_31_071814) do
+ActiveRecord::Schema.define(version: 2022_01_31_083606) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,6 +40,40 @@ ActiveRecord::Schema.define(version: 2022_01_31_071814) do
     t.index ["group_id"], name: "index_menu_items_on_group_id"
   end
 
+  create_table "order_items", force: :cascade do |t|
+    t.bigint "menu_item_id", null: false
+    t.bigint "order_id", null: false
+    t.integer "status"
+    t.integer "price_at_order"
+    t.text "request"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["menu_item_id"], name: "index_order_items_on_menu_item_id"
+    t.index ["order_id"], name: "index_order_items_on_order_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "table"
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
   add_foreign_key "menu_items", "categories"
   add_foreign_key "menu_items", "groups"
+  add_foreign_key "order_items", "menu_items"
+  add_foreign_key "order_items", "orders"
 end
