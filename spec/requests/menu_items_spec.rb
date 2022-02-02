ff1796062289178e3295aff313c534042352e77d
leighpoulier <86460742+leighpoulier@users.noreply.github.com>
2022-02-02 18:40:33 +1100

    fix linting of menu_items_spec.rb

--- spec/requests/menu_items_spec.rb
+++ spec/requests/menu_items_spec.rb
@@ -1,10 +1,12 @@
-require "rails_helper"
+# frozen_string_literal: true
 
-RSpec.describe "Menu Items", :type => :request do
-  it "indexes all menu items" do
+require 'rails_helper'
+
+RSpec.describe 'Menu Items', type: :request do
+  it 'indexes all menu items' do
     # headers = { "ACCEPT" => "application/json" }
-    get "/api/menu"
-    expect(response.content_type).to eq("application/json; charset=utf-8")
+    get '/api/menu'
+    expect(response.content_type).to eq('application/json; charset=utf-8')
     expect(response).to have_http_status(:ok)
     puts response.body
   end
