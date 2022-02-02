192a3120f4802c0e2fb7d4ef215efb1f65ba1c29
leighpoulier <86460742+leighpoulier@users.noreply.github.com>
2022-02-02 17:31:44 +1100

    still trying to get this continuous integration to work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'jwt_denylist'
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
