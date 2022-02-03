6515d73e382468cdc3ff362766a0c9bf90254684
leighpoulier <86460742+leighpoulier@users.noreply.github.com>
2022-02-03 12:47:31 +1100

    Rubocopped this time it should pass linting

--- db/migrate/20220203010749_add_quantity_to_order_items.rb
+++ db/migrate/20220203010749_add_quantity_to_order_items.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddQuantityToOrderItems < ActiveRecord::Migration[6.1]
   def change
     add_column :order_items, :quantity, :integer, default: 1, null: false
