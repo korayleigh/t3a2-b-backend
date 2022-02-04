916e74e6d61532a090c10a79fbcf38923de1360f
leighpoulier <86460742+leighpoulier@users.noreply.github.com>
2022-02-04 12:22:50 +1100

    add order update and delete actions

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Order < ApplicationRecord
-  has_many :order_items
+  has_many :order_items, dependent: :destroy
   has_many :menu_items, through: :order_items
 
   belongs_to :owner, class_name: 'User', optional: true
