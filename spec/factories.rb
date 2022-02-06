b180e41b735368339146181eae9385053fe3b07f
leighpoulier <86460742+leighpoulier@users.noreply.github.com>
2022-02-07 02:01:32 +1100

    Add factories for all models. Completed categories tests

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,11 +1,71 @@
 # frozen_string_literal: true
 
 FactoryBot.define do
-  sequence :name do |n|
-    "#{Faker::Food.ethnic_category}#{n}"
+  factory :category do
+    name { Faker::Food.unique.ethnic_category }
   end
 
-  factory :category do
-    name { generate :name }
+  factory :menu_item do
+    category
+    name { Faker::Food.unique.dish }
+    price { Faker::Number.between(from: 500, to: 10_000) }
+    description { Faker::Food.description }
+  end
+
+  tables = [ 'Table1', 'Table2', 'Table3', 'Table4', 'Table5', 'Table6', 'TakeAway']
+
+  factory :order do
+    table { tables.sample}
+    name { Faker::Name.name }
+    email { Faker::Internet.email }
+  end
+
+  factory :order_item do
+    menu_item
+    order
+  end
+
+  factory :menu_group do
+    name { Faker::Dessert.unique.variety }
+  end
+
+  factory :group_menu_item do
+    menu_group
+    menu_item
+    variant_name { Faker::Dessert.unique.flavor }
+  end
+
+  factory :permission do
+    name { Faker::Hacker.unique.ingverb }
+  end
+
+  factory :role do
+    name { Faker::Job.unique.position }
+  end
+
+  factory :permission_role do
+    role
+    permission
+  end
+
+  factory :customer
+
+  factory :employee do
+    role
+  end
+
+  password = Faker::Internet.password(min_length: 8)
+  factory :user do
+    email { Faker::Internet.unique.email }
+    password { password }
+    password_confirmation { password }
+    first_name { Faker::Name.first_name }
+    last_name { Faker::Name.last_name }
+    trait :for_customer do
+      association :userable, factory: :customer
+    end
+    trait :for_employee do
+      association :userable, factory: :employee
+    end
   end
 end
--- spec/requests/categories_spec.rb
+++ spec/requests/categories_spec.rb
@@ -4,9 +4,10 @@ require 'rails_helper'
 
 RSpec.describe 'Categories:', type: :request do
   describe 'index:' do
-    let!(:categories) { create_list(:category, 5) }
-
-    before { get '/api/categories' }
+    before do
+      create_list(:category, 5)
+      get '/api/categories'
+    end
 
     it 'responds with all categories' do
       expect(JSON.parse(response.body).size).to eq(5)
@@ -14,70 +15,73 @@ RSpec.describe 'Categories:', type: :request do
     it 'responds with status code 200' do
       expect(response).to have_http_status(:ok)
     end
+    it 'responds with content type: application/json' do
+      expect(response.content_type).to eq('application/json; charset=utf-8')
+    end
   end
 
   describe 'show:' do
+    let!(:category) { create(:category) }
     before do
-      @category = create(:category)
-      get "/api/categories/#{@category.id}"
+      get "/api/categories/#{category.id}"
     end
 
     it 'responds with the requested category' do
-      expect(JSON.parse(response.body)['name']).to eq(@category.name)
+      expect(JSON.parse(response.body)['name']).to eq(category.name)
     end
-
     it 'responds with status: ok' do
       expect(response).to have_http_status(:ok)
+    end
+    it 'responds with content type: application/json' do
+      expect(response.content_type).to eq('application/json; charset=utf-8')
     end
   end
 
   describe 'create:' do
+    let!(:new_category_attributes) { FactoryBot.attributes_for(:category) }
     before do
-      post '/api/categories', params: {
-        category: {
-          name: 'test_category'
-        }
-      }
+      post '/api/categories', params: { category: new_category_attributes }
     end
 
     it 'responds with the created category' do
-      expect(JSON.parse(response.body)['name']).to eq('test_category')
+      expect(JSON.parse(response.body, symbolize_names: true)).to include(**new_category_attributes)
     end
-
     it 'responds with status: created' do
       expect(response).to have_http_status(:created)
+    end
+    it 'responds with content type: application/json' do
+      expect(response.content_type).to eq('application/json; charset=utf-8')
     end
   end
 
   describe 'update:' do
+    let!(:category) { create(:category) }
+    let!(:new_category_attributes) { FactoryBot.attributes_for(:category) }
     before do
-      @category = create(:category)
-      @new_category_name = Faker::Food.ethnic_category
-      put "/api/categories/#{@category.id}",
-          params: { category: {
-            name: @new_category_name
-          } }
+      put "/api/categories/#{category.id}", params: { category: new_category_attributes }
     end
 
-    it 'updates a category name' do
-      expect(Category.find(@category.id).name).to eq(@new_category_name)
+    it 'updates category attributes' do
+      new_category_attributes.each do |attribute|
+        expect(Category.find(category.id)[attribute]).to eq(new_category_attributes[attribute])
+      end
     end
-
     it 'responds with status: created' do
       expect(response).to have_http_status(:ok)
+    end
+    it 'responds with content type: application/json' do
+      expect(response.content_type).to eq('application/json; charset=utf-8')
     end
   end
 
   describe 'delete:' do
+    let!(:category) { create(:category) }
     before do
-      @category = create(:category)
-      delete "/api/categories/#{@category.id}"
+      delete "/api/categories/#{category.id}"
     end
-
-    it 'deletes the requested question' do
-      expect { Category.find(@category.id) }.to raise_error(ActiveRecord::RecordNotFound)
+    it 'deletes the requested category' do
+      expect { Category.find(category.id) }.to raise_error(ActiveRecord::RecordNotFound)
     end
-
     it 'responds with status: no content' do
       expect(response).to have_http_status(:no_content)
     end
